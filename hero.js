32faa8859c5a7067c155b11dcfb58882777824da
krishnalathadeals5 <krishnalathadeals@gmail.com>
2026-07-02 22:28:53 +0530

    Update hero.js

--- hero.js
+++ hero.js
@@ -1,1 +1,23 @@
+const track = document.querySelector(".hero-track");
+const slides = document.querySelectorAll(".hero-slide");
 
+let index = 0;
+const total = slides.length;
+
+function moveSlide() {
+  track.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function autoSlide() {
+  index = (index + 1) % total;
+  moveSlide();
+}
+
+let slider = setInterval(autoSlide, 3000);
+
+const sliderContainer = document.querySelector(".hero-slider");
+
+sliderContainer.addEventListener("mouseenter", () => clearInterval(slider));
+sliderContainer.addEventListener("mouseleave", () => {
+  slider = setInterval(autoSlide, 3000);
+});
